9b0de9911f2c6de43c2c560df88ac136d0680266
shahid <shahidazhari313@gmail.com>
2024-05-20 15:31:45 +0530

    landing UI

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react"
+import { useRouter } from "next/router";
+import { v4 } from "uuid";
+import styles from "@/styles/home.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [roomId, setRoomId] = useState("")
 
-  const socket = useSocket()
+  const createAndJoin = () => {
+    const roomId = v4();
+    router.push(`/${roomId}`);
+  };
 
-  useEffect(() => {
-    socket?.on("connect", () => {
-      console.log(socket.id);
-    })
-  },[socket])
-  
+  const joinRoom = () => {
+    if (roomId) {
+      router.push(`/${roomId}`);
+    } else {
+      alert("Please provide a valid room ID")
+    }
+  }
+
   return (
-   <h1>Welcome</h1>
-  )
+    <div className={styles.homeContainer}>
+      <h1>Video Call App</h1>
+      <div className={styles.enterRoom}>
+        <input placeholder="Enter Room ID" value={roomId} onChange={(e) => setRoomId(e?.target?.value)} />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <span className={styles.separatorText}>
+        --------------- OR ---------------
+      </span>
+      <button onClick={createAndJoin}>Create a new Room</button>
+    </div>
+  );
 }
